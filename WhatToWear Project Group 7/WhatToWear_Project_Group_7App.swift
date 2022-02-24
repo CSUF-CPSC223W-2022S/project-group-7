49e88119a9049167c787ccd587d2ecea73ee17a9
andylasso25 <andylasso25@yahoo.com>
2022-02-24 08:16:34 -0800

    Initial Commit

---
+++ WhatToWear Project Group 7/WhatToWear_Project_Group_7App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToWear_Project_Group_7App.swift
+//  WhatToWear Project Group 7
+//
+//  Created by csuftitan on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatToWear_Project_Group_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
