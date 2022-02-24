93506789bf0aa6c290522939ec2c3c4138d212b1
andylasso25 <andylasso25@yahoo.com>
2022-02-24 08:55:14 -0800

    Initial Weather Commit

---
+++ weather_feature.swift
@@ -0,0 +1,12 @@
+//
+//  weather_feature.swift
+//  WhatToWear Project Group 7
+//
+//  Created by csuftitan on 2/24/22.
+//
+
+import Foundation
+
+struct weather {
+    // get an API or make up a weather pattern
+}
