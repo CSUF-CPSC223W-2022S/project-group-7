79937612526f5c770a12eaea00edc3e96dabff56
andylasso25 <andylasso25@yahoo.com>
2022-04-06 19:35:10 -0700

    Commit Check In 4

---
+++ View Controller.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Log In Project
+//
+//  Created by csuftitan on 4/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
