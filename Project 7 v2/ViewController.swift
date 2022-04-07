f9c6a805d8243142e6737c75e70f37e8d19c510b
andylasso25 <andylasso25@yahoo.com>
2022-04-07 09:11:51 -0700

    Initial Commit

---
+++ Project 7 v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 7 v2
+//
+//  Created by csuftitan on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
